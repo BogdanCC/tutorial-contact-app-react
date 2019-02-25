efe32cfc54a57e4fcc4b952d1a4dd4cc2732b3f2
BogdanCC <bogdan.e.corcoveanu@gmail.com>
2019-02-25 03:31:15 +0200

    Updates

---
+++ precache-manifest.48a7055d8e0f66ebef4bba8a9613c69e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed23685339ada1b6d88008cbe1a11e98",
+    "url": "/tutorial-contact-app-react/static/media/orb.ed236853.gif"
+  },
+  {
+    "revision": "dbd8beee3ebfb521186c",
+    "url": "/tutorial-contact-app-react/static/js/runtime~main.dbd8beee.js"
+  },
+  {
+    "revision": "a42c11717024132f86eb",
+    "url": "/tutorial-contact-app-react/static/js/main.a42c1171.chunk.js"
+  },
+  {
+    "revision": "1a0a8c5518d1148ade3e",
+    "url": "/tutorial-contact-app-react/static/js/2.1a0a8c55.chunk.js"
+  },
+  {
+    "revision": "a42c11717024132f86eb",
+    "url": "/tutorial-contact-app-react/static/css/main.47ca54d5.chunk.css"
+  },
+  {
+    "revision": "1a0a8c5518d1148ade3e",
+    "url": "/tutorial-contact-app-react/static/css/2.1cd36603.chunk.css"
+  },
+  {
+    "revision": "990786f2140ea1629074cdf02bb277a1",
+    "url": "/tutorial-contact-app-react/index.html"
+  }
+];
